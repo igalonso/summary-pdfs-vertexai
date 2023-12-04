0f6fcca95c3a3cf720cdd06a3bc25b276e5818c7
igalonso <igalonso@gmail.com>
2023-12-04 10:38:02 +0100

    docx replaced

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from langchain.chains.summarize import load_summarize_chain
 from langchain.llms import VertexAI
-from langchain.document_loaders import PyPDFLoader, WebBaseLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import Docx2txtLoader as DocxLoader
+
 from dotenv import load_dotenv
 load_dotenv()
 #from google.api_core.client_options import ClientOptions
@@ -8,11 +10,20 @@ load_dotenv()
 import os
 import json
 
-loader = PyPDFLoader("document.pdf")
-document = loader.load()
-docs = loader.load()
-llm = VertexAI(temperature=0.3, verbose=True, max_output_tokens=2047,model_name="text-bison-32k")
-chain = load_summarize_chain(llm, chain_type="map_reduce")
 
-summary = chain.run(docs)
+def summarize_docx(filename):
+    loader = DocxLoader(filename)
+    document = loader.load()
+    llm = VertexAI(temperature=0.3, verbose=True, max_output_tokens=2047, model_name="text-bison-32k")
+    chain = load_summarize_chain(llm, chain_type="map_reduce")
+    summary = chain.run(document)
+    return summary
+
+
+# loader = PyPDFLoader("document.pdf")
+# document = loader.load()
+# docs = loader.load()
+# llm = VertexAI(temperature=0.3, verbose=True, max_output_tokens=2047,model_name="text-bison-32k")
+# chain = load_summarize_chain(llm, chain_type="map_reduce")
+summary = summarize_docx("document.docx")
 print(summary)
